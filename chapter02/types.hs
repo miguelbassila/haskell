01b62198bb9137594d05a988ec8dd88ab4766c38
Miguel Bassila <miguelbassila@me.com>
2015-07-12 20:42:46 -0300

    Adds circumference with Float and Double

--- chapter02/types.hs
+++ chapter02/types.hs
@@ -3,3 +3,9 @@ removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 
 factorial :: Integer -> Integer
 factorial n = product[1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r

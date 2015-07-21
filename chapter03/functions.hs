cbf34f95c29254a0c09ab344f0daa541320c5cb0
Miguel Bassila <miguelbassila@me.com>
2015-07-20 22:20:26 -0300

    Add guards to tell BMI

--- chapter03/functions.hs
+++ chapter03/functions.hs
@@ -36,3 +36,9 @@ firstLetter "" = "Empty string, whoops!"
 firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
 -- Guards
+bmiTell :: Double -> String
+bmiTell bmi
+	| bmi <= 18.5 = "You're underweight, eat more!"
+	| bmi <= 25.0 = "Looking good!"
+	| bmi <= 30.0 = "You're overweight. Let's work out together!"
+	| otherwise = "You're obese. Go see a doctor."

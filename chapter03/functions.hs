b0908b90e51ce80b5ee621143a188cdbc2576068
Miguel Bassila <miguelbassila@me.com>
2015-07-26 20:16:40 -0300

    Add a function using let

--- chapter03/functions.hs
+++ chapter03/functions.hs
@@ -57,3 +57,15 @@ initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
 calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi w h | (w,h) <- xs]
 	where bmi weight height = weight / height ^ 2
+
+
+-- Let
+cylinder :: Double -> Double -> Double
+cylinder r h = 
+	let 
+		sideArea = 2 * pi * r * h
+		topArea = pi * r * 2
+	in sideArea + 2 * topArea
+
+calcBmis' :: [(Double, Double)] -> [Double]
+calcBmis' xs = [bmi | (w,h) <- xs, let bmi = w/h^2, bmi > 25.0]
